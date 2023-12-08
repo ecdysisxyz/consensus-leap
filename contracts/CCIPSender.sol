45d040a050af04741e6b0d0cb3796a13cf180268
0xHaku <32897506+0xHaku@users.noreply.github.com>
2023-12-08 23:11:02 +0530

    add ccisender

---
+++ contracts/CCIPSender.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
+import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+import {IERC20} from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.0/contracts/token/ERC20/IERC20.sol";
+
+contract CCIPSender {
+    enum PayFeesIn {
+        Native,
+        LINK
+    }
+
+    address immutable i_router;
+    address immutable i_link;
+
+    event MessageSent(bytes32 messageId);
+
+    constructor(address router, address link) {
+        i_router = router;
+        i_link = link;
+        IERC20(i_link).approve(i_router, type(uint256).max);
+    }
+
+    receive() external payable {}
+
+    function sendTrigger(
+        uint64 destinationChainSelector,
+        address receiver,
+        PayFeesIn payFeesIn
+    ) external returns (bytes32 messageId) {
+        Client.EVM2AnyMessage memory message = Client.EVM2AnyMessage({
+            receiver: abi.encode(receiver),
+            data: "",
+            tokenAmounts: new Client.EVMTokenAmount[](0),
+            extraArgs: "",
+            feeToken: payFeesIn == PayFeesIn.LINK ? i_link : address(0)
+        });
+
+        uint256 fee = IRouterClient(i_router).getFee(
+            destinationChainSelector,
+            message
+        );
+
+        if (payFeesIn == PayFeesIn.LINK) {
+            messageId = IRouterClient(i_router).ccipSend(
+                destinationChainSelector,
+                message
+            );
+        } else {
+            messageId = IRouterClient(i_router).ccipSend{value: fee}(
+                destinationChainSelector,
+                message
+            );
+        }
+
+        emit MessageSent(messageId);
+    }
+
+}
